19a644ed7f3e582a7f94483987f73b915af6b136
Raul Neto <raul.neto@tecnospeed.com.br>
2021-10-07 16:05:27 -0300

    feat: handling update and delete

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns ${id} coffee`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This actions updates a #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
+  }
+}
